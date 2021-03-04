7ce4a732bce7d31c2be960e9ed7af62f6a47a3cb
brianfreestone <brianfreestone@gmail.com>
2021-03-04 10:28:29 -0700

    Added Test

---
+++ tests/FortuneTellerTest.php
@@ -0,0 +1,26 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+require __DIR__ . "/../src/FortuneTeller.php";
+
+class FortuneTellerTest extends TestCase {
+
+
+   public function testRelationshipFortune_WithNothing_ReturnsString() {
+
+   }
+
+   public function testMoneyFortune_WithNothing_ReturnsNull() {
+
+   }
+
+   public function testFameFortune_WithNothing_Returns7() {
+
+   }
+
+   public function testLuckNumber_WithNothing_ReturnsWithinRange() {
+
+   }
+
+   
+}
